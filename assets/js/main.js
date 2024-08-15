3ed5cb561bbb6fb02154c5ad470e224fdcdf3f5a
AlexanderMckie <alex@sitara.net.au>
2024-08-15 12:41:28 +0800

    updated site

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function applyParallaxEffect(selector) {
         gsap.to(selector + ' .parallax', {
-            yPercent: 2, // Adjust this value to control the parallax effect
+            yPercent: 5, // Adjust this value to control the parallax effect
             ease: 'power1.inOut', // Add easing for smoother animation
             scrollTrigger: {
                 trigger: selector,
                 start: 'top bottom', // When the top of the container hits the bottom of the viewport
                 end: 'bottom top', // When the bottom of the container hits the top of the viewport
                 scrub: 1, // Smooth scrubbing with a delay of 1 second
-                markers: true // Optional: Add markers to visualize the start and end points
+                markers: false // Optional: Add markers to visualize the start and end points
             }
         });
     }
@@ -29,3 +29,17 @@ document.addEventListener('DOMContentLoaded', function() {
     applyParallaxEffect('.servicesTwo-image-container');
     applyParallaxEffect('.servicesOne-image-container');
 });
+
+// grows the textarea as the user types 
+document.addEventListener('input', function (event) {
+    if (event.target.tagName.toLowerCase() === 'textarea') {
+        autoGrow(event.target);
+    }
+});
+
+function autoGrow(element) {
+    element.style.height = 'auto';
+    element.style.height = (element.scrollHeight) + 'px';
+}
+
+// email service 
